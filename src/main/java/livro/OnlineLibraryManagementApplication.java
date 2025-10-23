40ddf415d63d132e00fe554823014c419353b120
BrunoChagas78 <121910005+BrunoChagas78@users.noreply.github.com>
2025-10-23 00:58:29 +0000

    ajustando pom.xml

--- src/main/java/livro/OnlineLibraryManagementApplication.java
+++ src/main/java/livro/OnlineLibraryManagementApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineLibraryManagementApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(OnlineLibraryManagementApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(OnlineLibraryManagementApplication.class, args);
+  }
 }
